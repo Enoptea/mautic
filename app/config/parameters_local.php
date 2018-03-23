d83de34c4bc027e80b5852c208f970b45980a1d1
Gaultier Boniface <gboniface@wysow.fr>
2018-03-23 16:02:16 +0100

    Add local config file.

---
+++ app/config/parameters_local.php
@@ -0,0 +1,14 @@
+<?php
+
+return $parameters = [
+    'api_enabled'           => true,
+    'db_driver'             => 'pdo_mysql',
+    'db_host'               => 'getenv(DB_HOST)',
+    'db_port'               => 'getenv(DB_PORT)',
+    'db_name'               => 'getenv(DB_NAME)',
+    'db_user'               => 'getenv(DB_USER)',
+    'db_password'           => 'getenv(DB_PASSWD)',
+    'db_table_prefix'       => 'getenv(MAUTIC_TABLE_PREFIX)',
+    'secret'                => 'getenv(MAUTIC_SECRET)',
+    'default_pagelimit'     => 10,
+];
